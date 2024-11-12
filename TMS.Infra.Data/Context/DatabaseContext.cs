0c25a836b4fc87c0e094916ab5794ca19e9a4868
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-11-12 16:43:15 -0300

    chore: app db context

---
+++ TMS.Infra.Data/Context/DatabaseContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using TMS.Infra.Data.Entities;
+
+namespace TMS.Infra.Data.Context
+{
+	public class DatabaseContext : DbContext
+	{
+		public DbSet<UserEntity> Users { get; set; }
+
+		public DatabaseContext(DbContextOptions options) : base(options)
+		{
+		}
+	}
+}
+
