f89788b7741cc9aeccf715abbf8cdecd1600fd53
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-11-12 16:06:13 -0300

    feat: login page

---
+++ TMS.Presentations/Pages/User/Login.razor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using TMS.Presentations.ViewModels.User;
+
+namespace TMS.Presentations.Pages.User
+{
+	public partial class Login
+	{
+		private readonly LoginViewModel _viewModel;
+
+		public Login()
+		{
+			_viewModel = new(); 
+		}
+
+		private void Submit()
+		{
+
+		}
+	}
+}
+
