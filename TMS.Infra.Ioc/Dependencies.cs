b24ea701fa16b4c5c3a6a6443fd5adba7422a79d
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-11-13 22:38:14 -0300

    chore: injecting db context

--- TMS.Infra.Ioc/Dependencies.cs
+++ TMS.Infra.Ioc/Dependencies.cs
@@ -1,12 +1,21 @@
 ﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using TMS.Infra.Data.Context;
 
 namespace TMS.Infra.Ioc
 {
 	public static class Dependencies
 	{
-		public static IServiceCollection AddExtensions(this IServiceCollection services) 
+		public static IServiceCollection AddExtensions(this IServiceCollection services, IConfiguration configuration) 
 		{
+			var conn = configuration.GetConnectionString("MSSQL") ?? throw new Exception("MSSQL CONN IS NULL");
+
+			services.AddDbContext<DatabaseContext>(
+				options => options.UseSqlServer(conn,
+					m => m.MigrationsAssembly(typeof(DatabaseContext).Assembly.FullName)));
+
 			return services;
 		}
 	}
