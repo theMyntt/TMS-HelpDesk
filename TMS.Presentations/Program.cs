c8af88e6080dc076f82dc98dc9a81d2cd8e6c6ab
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-11-13 22:29:51 -0300

    ci: add json file with enviroment name

--- TMS.Presentations/Program.cs
+++ TMS.Presentations/Program.cs
@@ -3,6 +3,9 @@ using Microsoft.AspNetCore.Components.Web;
 using TMS.Infra.Ioc;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: false, reloadOnChange: true);
+
 builder.Services.AddExtensions();
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
