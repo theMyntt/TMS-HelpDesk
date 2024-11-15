220dc520abf991e4271f1f9fdd6c87ebfc8d4d67
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-11-15 11:45:45 -0300

    chore: IUserRepository contract

---
+++ TMS.Infra.Data/Abstractions/IUserRepository.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+using System.Linq.Expressions;
+using TMS.Infra.Data.Entities;
+
+namespace TMS.Infra.Data.Abstractions
+{
+	public interface IUserRepository
+	{
+		Task CreateUser(UserEntity entity);
+		Task EditUser(UserEntity entity);
+		Task<IEnumerable<UserEntity>> GetUsers(int page);
+		Task<IEnumerable<UserEntity>> Filter(Expression<Func<UserEntity, bool>> filter, int page);
+		Task DeleteUser(UserEntity entity);
+	}
+}
+
