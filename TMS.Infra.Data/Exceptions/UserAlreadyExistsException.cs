516f70a0e5907705f0ba9e643cee839e17a4471c
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-11-15 11:45:28 -0300

    chore: user already exists exception

---
+++ TMS.Infra.Data/Exceptions/UserAlreadyExistsException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace TMS.Infra.Data.Exceptions
+{
+	public class UserAlreadyExistsException : Exception
+	{
+		public UserAlreadyExistsException() : base("User Already Exists")
+		{
+
+		}
+	}
+}
+
