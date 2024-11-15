a2846d766df3d29ba79bf841b4240cade777c860
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-11-15 11:46:07 -0300

    feat: create user implementation

---
+++ TMS.Infra.Data/Repositories/UserRepository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using TMS.Infra.Data.Abstractions;
+using TMS.Infra.Data.Context;
+using TMS.Infra.Data.Entities;
+using TMS.Infra.Data.Exceptions;
+
+namespace TMS.Infra.Data.Repositories
+{
+	public class UserRepository : IUserRepository
+	{
+        private readonly DatabaseContext _context;
+
+        public UserRepository(DatabaseContext context) => _context = context;
+
+        public async Task CreateUser(UserEntity entity)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Email == entity.Email);
+
+            if (user != null)
+            {
+                throw new UserAlreadyExistsException();
+            }
+
+            await _context.Users.AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public Task DeleteUser(UserEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task EditUser(UserEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<UserEntity>> Filter(Expression<Func<UserEntity, bool>> filter, int page)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<UserEntity>> GetUsers(int page)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
