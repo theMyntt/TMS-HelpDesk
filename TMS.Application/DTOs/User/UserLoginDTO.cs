b7f677e9da9ee53be303bc1bad76fe64a8ac407d
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-11-15 23:59:46 -0300

    chore: user login i/o

---
+++ TMS.Application/DTOs/User/UserLoginDTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using TMS.Domain.Core;
+
+namespace TMS.Application.DTOs.User
+{
+	public static class UserLoginDTO
+	{
+		public class Input
+		{
+			[Required]
+			[EmailAddress(ErrorMessage = "Email is invalid.")]
+			public required string Email { get; set; }
+
+			[Required]
+			[MinLength(6, ErrorMessage = "Password needs to be greater than 6.")]
+			public required string Password { get; set; }
+		}
+
+		public class Output : StandardResponse
+		{
+			public required string Token { get; set; }
+		}
+	}
+}
+
