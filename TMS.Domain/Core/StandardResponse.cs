a135f1480c7b84e1dd6c52bc4209b3ab436542bb
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-11-15 23:55:42 -0300

    chore: standard response method

---
+++ TMS.Domain/Core/StandardResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace TMS.Domain.Core
+{
+	public class StandardResponse
+	{
+		public required string Message { get; set; }
+		public required int StatusCode { get; set; }
+		public string? Code { get; set; }
+	}
+}
+
