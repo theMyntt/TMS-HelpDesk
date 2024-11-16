7bab64adc7955e41b4cdf27863e7afa4ea7425b4
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-11-16 14:44:13 -0300

    chore: IMapper core contract

---
+++ TMS.Domain/Core/IMapper.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace TMS.Domain.Core
+{
+	public interface IMapper<Aggregate, Entity> where Aggregate : AggregateRoot where Entity : class
+	{
+		Aggregate ToDomain(Entity entity);
+		Entity ToPersistance(Aggregate aggregate);
+	}
+}
+
