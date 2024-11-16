114b878cd7f270de36f5c4d961c194d094ad97be
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-11-16 14:44:41 -0300

    feat: user mapper

---
+++ TMS.Application/Abstractions/User/IUserMapper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using TMS.Domain.Aggregates;
+using TMS.Domain.Core;
+using TMS.Infra.Data.Entities;
+
+namespace TMS.Application.Abstractions.User
+{
+    public interface IUserMapper : IMapper<UserAggregate, UserEntity>
+    {
+	}
+}
+
---
+++ TMS.Application/Mappers/UserMapper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using TMS.Application.Abstractions.User;
+using TMS.Domain.Aggregates;
+using TMS.Infra.Data.Entities;
+
+namespace TMS.Application.Mappers
+{
+    public class UserMapper : IUserMapper
+	{
+		public UserMapper()
+		{
+		}
+
+        public UserAggregate ToDomain(UserEntity entity)
+        {
+            return new UserAggregate
+            {
+                Id = entity.Id,
+                Name = entity.Name,
+                Email = entity.Email,
+                Password = entity.Password,
+                CreatedAt = entity.CreatedAt,
+                UpdatedAt = entity.UpdatedAt
+            };
+        }
+
+        public UserEntity ToPersistance(UserAggregate aggregate)
+        {
+            return new UserEntity
+            {
+                Id = aggregate.Id,
+                Name = aggregate.Name,
+                Email = aggregate.Email,
+                Password = aggregate.Password,
+                CreatedAt = aggregate.CreatedAt,
+                UpdatedAt = aggregate.UpdatedAt
+            };
+        }
+    }
+}
+
